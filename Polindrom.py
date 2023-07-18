8c2c1fabb317e39756e4f9398dfd4920cbd76911
Владимир Чуб <savladimir1974@gmail.com>
2023-07-18 11:23:26 +0300

    ‘ИндексацияЦикл_for’

---
+++ Polindrom.py
@@ -0,0 +1,9 @@
+def IfPolindrom(word):
+    counter = 1
+    a = True
+    for i in range(0, len(word)//2):
+        if word[i] != word[i - counter]:
+            a = False
+        counter += 2
+    return a
+
